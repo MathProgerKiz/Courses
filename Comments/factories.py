4096d30b19d21986352eed8fffbda27090ba6350
MathProger <m.ibragimov271@mail.ru>
2024-08-08 03:54:06 +0300

    add file

--- Comments/factories.py
+++ Comments/factories.py
@@ -1,14 +1,1 @@
-import factory
-from factory.django import DjangoModelFactory
-from .models import UsersModel
-
-
-class UserFactory(DjangoModelFactory):
-    class Meta:
-        model = UsersModel
-
-    username = factory.Faker("user_name")
-    first_name = factory.Faker("first_name")
-    last_name = factory.Faker("last_name")
-    email = factory.Faker("email")
-    is_staff = True
+print(1111)
