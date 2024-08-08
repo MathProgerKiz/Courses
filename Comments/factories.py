6d7c88b1252c198afbc46bd2c98bc8779e2b7ba3
MathProger <m.ibragimov271@mail.ru>
2024-08-08 04:03:20 +0300

    Commit

--- Comments/factories.py
+++ Comments/factories.py
@@ -1,1 +1,15 @@
-print(1111)
+import factory
+from factory.django import DjangoModelFactory
+from .models import UsersModel
+
+
+class UserFactory(DjangoModelFactory):
+    """Класс для того чтобы генерировать различные данные для юзера"""
+    class Meta:
+        model = UsersModel
+
+    username = factory.Faker("user_name")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    email = factory.Faker("email")
+    is_staff = True
